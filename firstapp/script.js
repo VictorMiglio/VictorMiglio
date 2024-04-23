64b44b7e409b447db30635b69360162c72ce87dc
victormiglio <victormiglio@hotmail.com>
2024-04-23 16:24:39 -0300

    a

---
+++ firstapp/script.js
@@ -0,0 +1,15 @@
+//input
+anoNascimento = 1996
+nome = "victor"
+
+//processamento
+hoje = new Date()
+anoAtual = hoje.getFullYear()
+idade = anoAtual - anoNascimento
+mensagem = "Sou " +nome +" e tenho "  +idade+  " anos"
+
+//output
+alert(mensagem)
+
+
+
