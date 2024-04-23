699d409916e48894de19f3743c818863aefd6f66
victormiglio <victormiglio@hotmail.com>
2024-04-23 20:42:03 -0300

    adicionado preenchimento e soma

--- firstapp/script.js
+++ firstapp/script.js
@@ -1,15 +1,33 @@
+//constante
+const SALARIO_ATE_20 = 1000
+const SALARIO_ACIMA_20 = 2000
+
 //input
-anoNascimento = 1996
-nome = "victor"
+anoNascimento = parseInt(prompt("Informe o seu ano de nascimento"))
+nome = prompt("informe o seu nome")
+salarioBase = parseFloat(prompt("informe o seu salario base"))
+gratificacao = parseFloat(prompt("informe a gratificação"))
+bonus = parseFloat(prompt("informe o bônus"))
+desconto = parseFloat(prompt("informe o desconto"))
+
+salarioLiquido = 0
+adicional = 0
 
 //processamento
 hoje = new Date()
 anoAtual = hoje.getFullYear()
 idade = anoAtual - anoNascimento
-mensagem = "Sou " +nome +" e tenho "  +idade+  " anos"
+
+//TODO: tratarmos na proxima aula
+//ate 20 anos (inlcusive): 1000 SALARIO_ATE_10
+//acima de 20 anos: 2000 SALARIO_ACIMA_20ADICIONAL = SALARIO_ATE_20
+
+salarioLiquido = salarioBase + gratificacao + bonus - desconto + adicional
 
 //output
+mensagem = "Sou " + nome +", tenho " + idade + " anos e ganho R$" + salarioLiquido 
 alert(mensagem)
+
 
 
 
